db8a278fda71617446316f380cabc8f73c6a3588
Anton Valkouski <ruizave@gmail.com>
2022-11-04 18:42:03 +0300

    добавлен минимальный компонент структуры БД

---
+++ src/info-area-dbml.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import InfoArea from './components/InfoArea';
+
+function InfoAreaDbml() {
+  return (
+    <InfoArea areaLabel="dbml" caption="Структура БД" />
+  );
+}
+
+export default InfoAreaDbml;
