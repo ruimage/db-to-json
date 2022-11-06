cc5453d5688e8214fee6fed48e507f4edd5cca03
Anton Valkouski <ruizave@gmail.com>
2022-11-06 15:37:40 +0300

    добавлены вызовы библиотек конвертации

--- src/utils/converters.js
+++ src/utils/converters.js
@@ -1,5 +1,11 @@
-import { ModelExporter, Parser } from '@dbml/core';
+import {ModelExporter, Parser} from '@dbml/core';
+import jsonToTable from 'json-to-table';
+import {markdownTable} from 'markdown-table'
 
-const convertDBMLStringToJSON = (dbml) => ModelExporter.export(Parser.parse(dbml, 'dbml'), 'json', false);
+export const convertDBMLStringToJSON = (dbml) => ModelExporter.export(Parser.parse(dbml, 'dbml'), 'json', false);
 
-export default convertDBMLStringToJSON;
+export const convertJsonToArray = (json) => jsonToTable(json)
+
+export const convertArrayToMarkdownTable = array => markdownTable(array)
+
+export const convertJsonToMarkdown = json => markdownTable(jsonToTable(json))
